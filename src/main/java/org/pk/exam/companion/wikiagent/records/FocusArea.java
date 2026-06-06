b4269978cc087fa7a7c0f1000a5c99f105c2f0ff
Pravin Patil <prvnpatil11@gmail.com>
2026-06-06 15:49:59 +0530

    java records classes for wiki agent

---
+++ src/main/java/org/pk/exam/companion/wikiagent/records/FocusArea.java
@@ -0,0 +1,4 @@
+package org.pk.exam.companion.wikiagent.records;
+
+public record FocusArea(String topic) {
+}
---
+++ src/main/java/org/pk/exam/companion/wikiagent/records/FocusAreas.java
@@ -0,0 +1,7 @@
+package org.pk.exam.companion.wikiagent.records;
+
+
+import java.util.List;
+
+public record FocusAreas(List<FocusArea> focusAreas) {
+}
